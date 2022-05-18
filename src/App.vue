7010a3cf8d52a6928dfa407089e27d4af76e8b83
Rail <wanglifei@yiqicai.com>
2022-05-18 19:33:34 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import VueDragResize from './index';
+
+</script>
+<template>
+  <div class="container">
+    <section>
+      <VueDragResize />
+    </section>
+    <section>
+      <VueDragResize />
+    </section>
+  </div>
+</template>
